a14b73baf7b1eb13a7193493672e6fbbb6c15c28
= <=>
2025-12-16 19:35:03 +0530

    Update frontend files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-const App = () => {
+import Home from './pages/Home';
+import Collection from './pages/collection';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import PlaceOrder from './pages/PlaceOrder';
+import Order from './pages/Orders'; 
+
+import Navbar from './components/Navbar';
+
+function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Tailwind is Working!
-      </h1>
+    <div className="px-4 sm:px-[5vw] lg:px-[9vw]">
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Order />} />
+      </Routes>
     </div>
   );
 }
