e3c9b9b7544bb576e2670e6190860e297bf6b3b5
sandun18 <sandunanjana333@gmail.com>
2025-12-17 13:14:33 +0530

    add hr tag

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,6 +11,7 @@ const Navbar = () => {
 
             <NavLink className='flex flex-col items-center gap-1'>
                 <p>Home</p>
+                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700'/>
             </NavLink>
 
         </ul>
